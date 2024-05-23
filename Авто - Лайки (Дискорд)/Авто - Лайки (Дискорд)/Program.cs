1d6a10ea19d2c853dccd878f0de9d15caf8120ae
Svayt_ <Svayt_@DESKTOP-0CR5UA6>
2024-05-24 00:22:39 +0300

    Добавьте файлы проекта.

---
+++ Авто - Лайки (Дискорд)/Авто - Лайки (Дискорд)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace Авто___Лайки__Дискорд_
+{
+    class Program
+    {
+        DiscordSocketClient client;
+        private ulong userId;
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            client.MessageReceived += CommandsHandler;
+            client.Log += Log;
+
+            var token = "MTI0MzIxMzE2Njk0MzY3MDI4Mg.GXZuM5.7Xhl3_M7bl8DITR-alL9lGLYNpr7Mz7tqJkGaE";
+            userId = 486577953824702503;
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            Console.ReadLine();
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task CommandsHandler(SocketMessage message)
+        {
+            if (message.Author.IsBot) return;
+
+             if (message.Author.Id == userId)
+            {
+                Emoji emoji = new Emoji("👽");
+                await message.AddReactionAsync(emoji);
+            }
+        }
+    }
+}
